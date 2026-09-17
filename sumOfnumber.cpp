bf813284d21a828a4b2a3e91ef96ed2540be0d7e
Abhishek962127 <abhishek6666555@gmail.com>
2026-09-17 10:28:35 +0530

    lab

---
+++ sumOfnumber.cpp
@@ -0,0 +1,18 @@
+//Sum of digits of a number by recursion
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "Sum of digits: " << sumOfDigits(number) << endl;
+    return 0;
+}
